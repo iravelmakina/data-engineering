b5bb495cc0d8038fea12e939bf2276ea30356334
Iryna Velmakina <i.velmakina@headway.inc>
2026-04-19 14:41:16 +0300

    chore: remove redundant dir

--- 02_assignment/food_delivery_dbt/tests/marts/fct_deliveries_positive_duration_on_delivery.sql
+++
@@ -1,7 +0,0 @@
--- Test: fct_deliveries delivery_duration_min should be positive when delivery_status is 'delivered'
-select
-  delivery_id,
-  delivery_duration_min,
-  delivery_status
-from {{ ref('fct_deliveries') }}
-where delivery_status = 'delivered' and delivery_duration_min <= 0
--- 02_assignment/food_delivery_dbt/tests/marts/fct_order_items_line_total_calculation.sql
+++
@@ -1,8 +0,0 @@
--- Test: fct_order_items line_total_usd should equal quantity * unit_price_usd
-select
-  order_item_id,
-  quantity,
-  unit_price_usd,
-  line_total_usd
-from {{ ref('fct_order_items') }}
-where round(line_total_usd, 2) != round(quantity * unit_price_usd, 2)
